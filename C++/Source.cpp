4b93991d6ad6ca050ca902d7bda2daafe65219f7
Sasha Glushko <potroshitelv2.0@gmail.com>
2019-10-07 19:04:30 +0300

    добавлена M_PI

--- C++/Source.cpp
+++ C++/Source.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <cstdlib> // для system
 #include <conio.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 using namespace std;
 
 int main()
 {
 	cout << "Hello, world!" << endl;
 	
+	cout << M_PI << endl;
+	
 	_getch();
-	return 0;
+	
 }
