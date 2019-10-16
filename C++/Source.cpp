4d0c83f5d8db195ac6fd1af6375b86a5702a9cc8
SasPK <SasPK@DESKTOP-LC7H2OF>
2019-10-16 11:25:51 +0300

    Все задания

--- C++/Source.cpp
+++ C++/Source.cpp
@@ -10,14 +10,40 @@ using namespace std;
 int main()
 {
 	
-
+	/*Задание_1*/
+	double x;
+	int n,s=0;
+	double y;
 	
-	for (int i = 1; i <=10; i++)
+	cout << " x: ";
+	cin >> x;
+
+	cout << " n: ";
+	cin >> n;
+
+	for (int i = 0; i <= n; i++)
 	{
-		for (int g = 1; g < 10; g++)
-		{
-			cout << g << "*" << i << "=" << g * i << endl;
-		}
+		y = pow(x, 3 * i);
+		s = s + y;
+		
+	}
+	cout << s << endl;
+	
+	/*Задание_2*/
+	double P, M, N;
+	cin >> M >> N;
+	for (int i = 0; i < M; i++)
+	{
+		P= N * M;
+	}
+	cout << "P=: " << P << endl;
+	
+	
+	/*ЗАДАНИЕ_3*/
+	
+	for (int i = 0; i < 1; i+=0.1)
+	{
+		cout<< tan(i) << endl;
 	}
 
 
