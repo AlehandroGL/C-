c32ccf4908123442f02c87e09a6e9c5b22731ae9
SasPK <SasPK@DESKTOP-LC7H2OF>
2019-11-26 18:09:46 +0300

    лаба_7

--- C++/Source.cpp
+++ C++/Source.cpp
@@ -6,66 +6,38 @@
 #include <math.h>
 
 using namespace std;
+/*
+
+float maxx(float a, float b)
+{
+	if a>b
+		return a
+	if a<b
+		return b
+}
+
+
+
+float foo_1(float p, float t)
+{
+	return sqrt(p+t*t)
+}
+
+float foo_2(float k, float p)
+{
+	return p + k;
+}
+*/
+
+
 
 int main()
 {
 	setlocale(LC_ALL, "RUS");
 	
-	/*1 задание*/
-	double x, f, u, S = 0;
-	int i, k = 0;
-
-	cin >> x;
-
-	do
-	{
-		k++;
-		for (int i = 0; i <= (2 * k); i++)
-		{
-			f *= i;
-			u = (pow(-1, k - 1) * pow(x, 3 * k - 1)) / ((2 * k) * f);
-			S += u;
-		}
-	} while (fabs(u) > 1e-4);
+	//y = pow(atan(x), 2);
 	
-		cout << S <<endl;
-		cout << k << endl;
-	
+	foo(345);
 
-
-
-	/* zadanie 2*/
-	double result;
-	double x, k,F;
-	int n;
-
-	cin >> x;
-	cin >> k;
-	cin >> n;
-	do {
-		for (int i = 0; i < n; i++)
-		{
-			F = (5 * log(2 * k * x)) / atan(2 * x) + pow(k, 2);
-			result += F;
-		}
-
-
-
-	} while (i <= n);
-	cout << F << endl;
-	
-	
-	
-	/*3 задание*/
-	double value;
-	double value_result;
-	cin >> value;
-
-	do
-	{
-		value_result=
-	} while (true);
-	cout << value_result;
 	_getch();
-	
 }
